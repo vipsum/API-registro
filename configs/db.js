a8e1214e2d552f5b23b73cabbdfaa2d4f023ce0b
vipsum <83631487+vipsum@users.noreply.github.com>
2022-06-04 18:31:05 -0300

    heroku db

--- configs/db.js
+++ configs/db.js
@@ -2,11 +2,13 @@
 const { Sequelize } = require("sequelize");
 //setting database name, username, and password
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || "postgres://postgres:postgres@127.0.0.1:5432/registerdb", {
-  ssl: {
-    "require": "false"
-  }
-}
-);
+  process.env.DATABASE_URL || "postgres://postgres:postgres@127.0.0.1:5432/registerdb", (process.env.DATABASE_URL) ? {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  } : {});
 
 module.exports = sequelize;
