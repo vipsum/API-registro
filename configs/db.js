28faa99882a575566d5a0ed372dee03b6c7be004
vipsum <83631487+vipsum@users.noreply.github.com>
2022-06-04 18:22:06 -0300

    heroku db

--- configs/db.js
+++ configs/db.js
@@ -4,8 +4,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DATABASE_URL || "postgres://postgres:postgres@127.0.0.1:5432/registerdb", {
   ssl: {
-    "require": "true",
-    "rejectUnauthorized": "false"
+    "require": "false"
   }
 }
 );
